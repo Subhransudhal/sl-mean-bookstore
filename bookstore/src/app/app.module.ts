64e428cf6ed1c2062d5aa1887ad041d28af673e6
subhransudhal <rkl_subhransu@yahoo.in>
2022-01-20 20:45:18 -0800

    login

--- bookstore/src/app/app.module.ts
+++ bookstore/src/app/app.module.ts
@@ -2,13 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AuthModule } from './auth/auth.module';
+import { LoginComponent } from './auth/compnents/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
